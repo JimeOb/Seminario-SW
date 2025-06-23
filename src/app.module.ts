f423e1aa7a2cfd96a891bb59a7fde4984eb127c7
CamiloRodriguezG <72152781+CamiloRodriguezG@users.noreply.github.com>
2025-06-22 22:31:14 -0500

    inicio

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SolicitudesModule } from './solicitudes/solicitudes.module';
+import { ReservasModule } from './reservas/reservas.module';
+import { PrestamosModule } from './prestamos/prestamos.module';
+import { RecursosModule } from './recursos/recursos.module';
+import { ValoracionesModule } from './valoraciones/valoraciones.module';
+import { HorariosModule } from './horarios/horarios.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'host',
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+      username: process.env.DB_USERNAME || 'usuario',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'test',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Nota: No usar en producción
+    }),
+    UsuariosModule,
+    SolicitudesModule,
+    ReservasModule,
+    PrestamosModule,
+    RecursosModule,
+    ValoracionesModule,
+    HorariosModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/valoraciones/valoraciones.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('valoraciones')
+export class ValoracionesController {}
